a338729edd5e4b096924cf10e78626eaf598a8ba
Zeha Irawan <jangkaringbumi@gmail.com>
2019-01-13 22:42:47 +0700

    basic accordion for shop page

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 //<!--SHOW/HIDE UL LIST
-    function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';      
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
+  });
+}
 //-->
